45365416c8533a5fbfd64daef907e5e6676cac59
junesdream <norisori@gmail.com>
2024-10-16 00:08:08 +0200

    Add relationships between models

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    
+    // Define the relation to Company-Modell
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    // Define the relation tp Category-Modell
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
